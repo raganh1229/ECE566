4b763c81b86a5582fc15a3e52dfe3937da58755a
James Tuck <jtuck@ncsu.edu>
2021-01-24 09:59:33 -0500

    Remove use of pushd and popd. Don't know why it's not working.

--- travis-ci-test.sh
+++ travis-ci-test.sh
@@ -1,9 +1,6 @@
 set -e
 clang-11 --version
-make -C LectureCode all
-pushd .
-cd projects/p0 && mkdir build && cd build && cmake -DLLVM_DIR=/usr/local/lib/cmake/llvm .. && cmake --build .
-popd
-pushd .
-mkdir p0-test && cd p0-test && ../wolfbench/configure && make all 
-popd
+cd /ece566 && make -C LectureCode all
+cd /ece566/projects/p0 && rm -rf build && mkdir build && cd build && cmake -DLLVM_DIR=/usr/local/lib/cmake/llvm .. && cmake --build .
+cd /ece566 && rm -rf p0-test && mkdir p0-test && cd p0-test && ../wolfbench/configure && make all 
+
